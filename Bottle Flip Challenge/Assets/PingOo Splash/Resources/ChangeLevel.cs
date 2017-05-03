a357fa813f32e87cb33b90eaec0a6da4c11df498
Hadid <blunty.hadid@gmail.com>
2017-05-03 06:53:12 +0500

    Commit-6
    Addition

---
+++ Bottle Flip Challenge/Assets/PingOo Splash/Resources/ChangeLevel.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLevel : MonoBehaviour
+{
+
+    public AudioSource games;
+    public AudioSource tag;
+    public AudioSource pop;
+
+    void Start()
+    {
+        Invoke("changeLevel", 5.0f);
+    }
+
+
+    void changeLevel()
+    {
+        Application.LoadLevel(1);
+    }
+
+    void playAudioGames()
+    {
+        games.Play();
+    }
+
+    void stopAudioGames()
+    {
+        games.Stop();
+    }
+
+    void playAudioTag()
+    {
+        tag.Play();
+    }
+    void stopAudioTag()
+    {
+        tag.Stop();
+    }
+
+    void playAudiopop()
+    {
+        pop.Play();
+    }
+    void stopAudiopop()
+    {
+        pop.Stop();
+    }
+}
